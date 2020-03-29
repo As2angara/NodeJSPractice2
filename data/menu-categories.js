2f7369660cf440a2e9c8dd1ffda9cfbdda35fd39
AdrianSA <Adrian.angara@gmail.com>
2020-03-29 01:47:14 -0400

    added menu items and menu categories module

---
+++ data/menu-categories.js
@@ -0,0 +1,21 @@
+
+const categories = [
+  {
+    "id": 1,
+    "name": "Sandwiches & Wraps"
+  },
+  {
+    "id": 2,
+    "name": "Entrees"
+  },
+  {
+    "id": 3,
+    "name": "Sides"
+  },
+  {
+    "id": 4,
+    "name": "Desserts"
+  }
+]
+
+module.exports = categories;
---
+++ data/menu-items.js
@@ -0,0 +1,131 @@
+const items = [
+  {
+    "id": 1,
+    "name": "Grilled Portobello Sandwich",
+    "price": 14.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 1
+  },
+  {
+    "id": 2,
+    "name": "Grilled Chicken Sandwich",
+    "price": 10.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 1
+  },
+  {
+    "id": 3,
+    "name": "Gyro wrap",
+    "price": 9.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 1
+  },
+  {
+    "id": 4,
+    "name": "Falafel Wrap",
+    "price": 9.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 1
+  },
+  {
+    "id": 5,
+    "name": "Wagyu Burger",
+    "price": 50.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 1
+  },
+  {
+    "id": 6,
+    "name": "Penne Pasta",
+    "price": 12.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 2
+  },
+  {
+    "id": 7,
+    "name": "10' Pizza with 2 toppings",
+    "price": 12.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 2
+  },
+  {
+    "id": 8,
+    "name": "Spaghetti and Meatballs",
+    "price": 12.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 2
+  },
+  {
+    "id": 9,
+    "name": "9 piece Chicken wings",
+    "price": 10.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 2
+  },
+  {
+    "id": 10,
+    "name": "Chicken thigh with seasoned rice",
+    "price": 12.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 2
+  },
+  {
+    "id": 11,
+    "name": "French Fries",
+    "price": 5.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 3
+  },
+  {
+    "id": 12,
+    "name": "Mac n' Cheese",
+    "price": 5.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 3
+  },
+  {
+    "id": 13,
+    "name": "Caesar Salad",
+    "price": 5.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 3
+  },
+  {
+    "id": 14,
+    "name": "Creamy Coleslaw",
+    "price": 5.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 3
+  },
+  {
+    "id": 15,
+    "name": "Chocolate cake",
+    "price": 4.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 4
+  },
+  {
+    "id": 16,
+    "name": "Chocolate Croissant",
+    "price": 3.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 4
+  },
+  {
+    "id": 17,
+    "name": "Upside-down Pineapple Cake",
+    "price": 4.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 4
+  },
+  {
+    "id": 18,
+    "name": "Brownies",
+    "price": 3.00,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis consequat neque sed varius. Cras fringilla quam lacus, eu tristique.",
+    "catid": 4
+  }
+
+]
+
+module.exports = items;
