73d0252b148a4ae3d8abfd9dcd344d1284c0882c
AdrianSA <Adrian.angara@gmail.com>
2020-03-30 20:40:52 -0400

    Added item, categories, and index routes

---
+++ routes/categories.js
@@ -0,0 +1,51 @@
+const categories = require('./data/menu-categories');
+const express = require('express');
+const router = express.Router();
+
+
+//CREATE
+router.post('/create', (req, res) => {
+
+    const category = {
+        id: categories.length + 1,
+        name: req.body.name
+    }
+
+    categories.push(category);
+
+    res.send(categories);
+
+});
+
+//READ
+router.get('/read', (req, res) => {
+    res.send(categories);
+});
+
+//UPDATE
+router.put('/update/:id', (req, res) => {
+
+    const category = categories.find(c => c.id = parseInt(req.params.id));
+
+    category.name = req.body.name;
+
+    res.send(categories);
+
+});
+
+//DELETE
+router.delete('/delete/:id', (req, res) => {
+
+    const category = categories.find(c => c.id = parseInt(req.params.id));
+
+    const index = categories.indexOf(category);
+
+    categories.splice(index, 1);
+
+    res.send(categories);
+
+});
+
+
+module.exports = router;
+
---
+++ routes/items.js
@@ -0,0 +1,56 @@
+const items = require('./data/menu-items');
+const express = require('express');
+const router = express.Router();
+
+//CREATE
+router.post('/create', (req, res) => {
+
+    const item = {
+        id: items.length + 1,
+        name: req.body.name,
+        price: req.body.price,
+        desc: req.body.desc,
+        catid: req.body.catid
+    }
+
+    items.push(item);
+
+    res.send(items);
+
+});
+
+
+
+//READ
+router.get('/read', (req, res) => {
+    res.send(items);
+});
+
+
+
+//UPDATE
+router.put('/update/:id', (req, res) => {
+
+    const item = items.find(i => i.id = parseInt(req.params.id));
+
+    item.name = req.body.name;
+
+    res.send(items);
+
+});
+
+//DELETE
+router.delete('/delete/:id', (req, res) => {
+
+    const item = items.find(i => i.id = parseInt(req.params.id));
+
+    const index = items.indexOf(item);
+
+    items.splice(index, 1);
+
+    res.send(items);
+
+});
+
+
+module.exports = router;
